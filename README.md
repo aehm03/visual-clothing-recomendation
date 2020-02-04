5763e852fac8e370992cb0b528caf7a7b966ae65
Moritz Wilke <wilke@informatik.uni-leipzig.de>
2020-02-04 17:29:52 +0100

    initialized backend apis with mock-responses

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vip-retrieval-demo
 
+test api with curl: `curl -F file=@test.jpg  http://localhost:5000/api/detect`
---
+++ api/app.py
@@ -0,0 +1,91 @@
+from flask import Flask, Response, jsonify, request
+from flask_cors import CORS
+from PIL import Image
+
+# configuration
+DEBUG = True
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+# TODO replace cors with other method for vue frontend
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# TODO which kind of data are the requests, form, json, wat?
+
+@app.route('/')
+def frontend():
+    """
+    Serves the frontend of the application.
+    :return: frontend index.html
+    """
+    return 'hello, wordl'
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/api/detect', methods=['POST'])
+def detection():
+    """
+    detects objects categories and their position in the posted image.
+    :return: list of detected objects and bounding boxes
+    """
+    if 'file' not in request.files:
+        return Response('[DETECT]  no file part', status=400)
+
+    file = request.files['file']
+
+    # if user does not select file, browser may submit an empty part without filename
+    if file.filename == '':
+        return Response('[DETECT] empty file part', status=400)
+
+    if file and not allowed_file(file.filename):
+        return Response(file.filename + '[DETECT] is not in allowed extensions ' + ALLOWED_EXTENSIONS, status=400)
+
+    img = Image.open(file.stream)
+
+    # Currently we return a box that covers the whole image and contains a short_sleeve_top
+    return jsonify({'items': [{'category': 'short sleeve top', 'box': [0, 0, *img.size]}]})
+
+
+@app.route('/api/match/categories')
+def categories():
+    """
+    Returns all available product categories
+    TODO matching only works with "short sleeve tops"s!
+    :return:  { categories: [(string)]}
+    """
+    return jsonify({'categories': [{'short sleeve top',
+                                    'trousers',
+                                    'short sleeve dress',
+                                    'shorts',
+                                    'sling dress',
+                                    'sling',
+                                    'long sleeve outwear',
+                                    'long sleeve top',
+                                    'skirt',
+                                    'vest dress',
+                                    'long sleeve dress',
+                                    'vest',
+                                    'short sleeve outwear'}]})
+
+
+@app.route('/api/match', methods=['POST'])
+def match():
+    """
+    Matches the given product with the catalog and returns n similar products
+    :return:
+    """
+
+    return jsonify({'matches': [{'id': 123}, {'id': 456}]})
+
+
+if __name__ == '__main__':
+    app.run()
